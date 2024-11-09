6d23c512bf9309f1c218069c482b5a6fe477fb74
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-11-09 15:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function debounce(callback, delay, immediate = false) {
-  
+	  let timeOut;
+	
+	  return function(...args) {
+		  const context = this;
+		  const callNow = immediate && !timeOut;
+
+		  clearTimeout(timeOut);
+
+		  timeOut = setTimeout(() => {
+			  timeOut = null;
+			  if(!immediate) {
+				  callback.apply(context, args);
+			  }
+		  }, delay);
+
+		  if(callNow) callback.apply(context, args);
+	  }
   }
   
   module.exports = debounce;
